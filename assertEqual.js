cbbb9bfb387115052d1df01331aded2594821999
Ralph Unrau <xralphunrau@gmail.com>
2022-03-07 23:58:41 +0000

    assert equal p1

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+let assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("hectic", "hectix");
+assertEqual(1,1);

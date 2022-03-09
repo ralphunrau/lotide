0bf94d9233d967ad56922a5c5ac7fcd7b40179b1
Ralph Unrau <xralphunrau@gmail.com>
2022-03-09 22:49:02 +0000

    Letter Positions!

---
+++ letterPositions.js
@@ -0,0 +1,39 @@
+let letterPositions = function(sentence) {
+  const results = {}
+  index = 0;
+  for (const item of sentence) {
+    if (results[item]) {
+      results[item].push(index);
+    } else if (item === ' '){
+    } else {
+      results[item] = [index];
+    }
+    index++;
+  }
+  return results;
+}
+
+
+
+function eqArrays(arr1, arr2) {
+  let longerArray = arr1;
+  if (arr2.length > arr1.length) {
+    longerArray = arr2;
+  }
+  for (let x = 0; x < longerArray.length; x++) {
+    if (arr1[x] !== arr2[x]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+function assertArraysEqual(arr1, arr2) {
+  if (eqArrays(arr1,arr2)) {
+    console.log(`Assertion Passed: ${arr1} === ${arr2}!`);
+  } else {
+    console.log(`Assertion Failed: ${arr1} !== ${arr2}!`);
+  }
+};
+
+assertArraysEqual(letterPositions("hello").o, [4]);

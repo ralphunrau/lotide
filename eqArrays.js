1f02ccae6c2e4711531ce4d2d22d0a50cca08853
Ralph Unrau <xralphunrau@gmail.com>
2022-03-14 20:36:29 +0000

    Implementing module exports to lotide.

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-let assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}!`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}!`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
@@ -16,4 +10,6 @@ const eqArrays = function(arr1, arr2) {
     }
   }
   return true;
-}
+};
+
+module.exports = eqArrays;
---
+++ test/eqArraysTest.js
@@ -0,0 +1,4 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+assertEqual(eqArrays([1,2],[1,2]), true)

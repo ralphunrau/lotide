c09a1448ffd5485e59838dfeaceb07f990975faa
Ralph Unrau <xralphunrau@gmail.com>
2022-03-14 20:44:18 +0000

    Implementing module exports to lotide.

--- middle.js
+++ middle.js
@@ -1,25 +1,6 @@
-function eqArrays(arr1, arr2) {
-  let longerArray = arr1;
-  if (arr2.length > arr1.length) {
-    longerArray = arr2;
-  }
-  for (let x = 0; x < longerArray.length; x++) {
-    if (arr1[x] !== arr2[x]) {
-      return false;
-    }
-  }
-  return true;
-};
+const assertArraysEqual = require('./assertArraysEqual')
 
-function assertArraysEqual(arr1, arr2) {
-  if (eqArrays(arr1,arr2)) {
-    console.log(`Assertion Passed: ${arr1} === ${arr2}!`);
-  } else {
-    console.log(`Assertion Failed: ${arr1} !== ${arr2}!`);
-  }
-};
-
-function middle(arr) {
+const middle = function(arr) {
   if (arr.length < 3) {
     return [];
   } else if (arr.length % 2 === 1) {
@@ -29,4 +10,4 @@ function middle(arr) {
   }
 };
 
-console.log(assertArraysEqual(middle([3,4,5,6]),[4,5]));
+module.exports = middle;
---
+++ test/testMiddle.js
@@ -0,0 +1,4 @@
+const middle = require('../middle');
+const assertArraysEqual = require('../assertArraysEqual');
+
+assertArraysEqual(middle([3,4,5,6]),[4,5]);
